eb69797092531b87089e762f57e5133c6f171a13
Realisimus <oo@oo.oo>
2014-12-18 15:40:09 +0200

    web 1.0

---
+++ src/main/java/game/NumbersLibrary.java
@@ -0,0 +1,54 @@
+package game;
+
+import java.util.*;
+
+public class NumbersLibrary {
+
+    private ArrayList<char[]> numbers = new ArrayList<char[]>();
+
+    public NumbersLibrary(int n) {
+        for (int i = (int) Math.pow(10, n - 1); i < Math.pow(10, n); i++) {
+            if (differ(i)) {
+                numbers.add(String.valueOf(i).toCharArray());
+            }
+        }
+    }
+
+    private Boolean differ(int i) {
+        Set set = new HashSet();
+        for (char c : String.valueOf(i).toCharArray()) set.add(c);
+        return set.size() == String.valueOf(i).length();
+    }
+
+    public void clean(char[] number, char[] bc) {
+        int i = 0;
+        while(i < numbers.size()) {
+            char[] c = calc(number, numbers.get(i));
+            if (c[0] != bc[0] || c[1] != bc[1]) numbers.remove(i);
+                else i++;
+        }
+        numbers.trimToSize();
+    }
+
+    public char[] random() {
+        Random rand = new Random();
+        int R = rand.nextInt(numbers.size());
+        return String.valueOf(numbers.get(R)).toCharArray();
+    }
+
+    public char[] calc(char[] number1, char[] number2) {
+        int bulls = 0;
+        int cows = 0;
+        for (int i = 0; i < number1.length; i++) {
+            for (int j = 0; j < number2.length; j++) {
+                if (number1[i] == number2[j]) {
+                    if (i == j) bulls++;
+                        else cows++;
+                }
+            }
+        }
+        String bc = Integer.toString(cows) + Integer.toString(bulls);
+        return bc.toCharArray();
+    }
+
+}
